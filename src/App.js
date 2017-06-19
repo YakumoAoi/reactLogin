c367f00be9c43ded00c53f87a2c71a655134ef03
YakumoAoi <416910533@qq.com>
2017-06-19 12:05:47 +0900

    delete demo

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ import {getCurrentUser,logOut} from './leanCloud'
 import deepCopy from './deepCopy'
 import AV from './leanCloud'
 
- var TodoFolder = AV.Object.extend('TodoFolder');
-  // 新建对象
-  var todoFolder = new TodoFolder();
-  // 设置名称
-  todoFolder.set('name','工作');
-  // 设置优先级
-  todoFolder.set('priority',1);
-  todoFolder.save().then(function (todo) {
-    console.log('objectId is ' + todo.id);
-  }, function (error) {
-    console.error(error);
-  });
-
 class App extends Component {
 	constructor(props){
 		super(props)
